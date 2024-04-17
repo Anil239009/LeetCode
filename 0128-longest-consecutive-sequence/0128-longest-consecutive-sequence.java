1bcdc3aa70010a5855a9b422fc8be98260cd955c
Anil Patil <147407317+Anil239009@users.noreply.github.com>
2024-04-17 12:12:01 +0530

    Time: 25 ms (77.72%), Space: 68 MB (5.30%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,24 +1,47 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length==0){
-            return 0;
+        // if(nums.length==0){
+        //     return 0;
+        // }
+        // Arrays.sort(nums);
+        // int max_Length=1;
+        // int prev_element=nums[0];
+        // int length=1;
+        // for(int i=0; i<nums.length;i++){
+        //     if(prev_element==nums[i]){
+        //         continue;
+        //     }
+        //     if(nums[i]==(prev_element+1)){
+        //         length++;
+        //     }else{
+        //         length=1;
+        //     }
+        //     prev_element=nums[i];
+        //     max_Length=Math.max(max_Length,length);
+        // }
+        // return max_Length;
+        
+        int n=nums.length;
+        if(n==0) return 0;
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0; i<n;i++){
+            hs.add(nums[i]);
         }
-        Arrays.sort(nums);
-        int max_Length=1;
-        int prev_element=nums[0];
-        int length=1;
-        for(int i=0; i<nums.length;i++){
-            if(prev_element==nums[i]){
-                continue;
+        int max_length=1;
+        for(Integer element:hs){
+            int prev=element-1;
+            if(!hs.contains(prev)){
+                int length=1;
+                int next=element+1;
+                while(hs.contains(next)){
+                    length++;
+                    next++;
+                }
+                 max_length= Math.max(max_length,length);
             }
-            if(nums[i]==(prev_element+1)){
-                length++;
-            }else{
-                length=1;
-            }
-            prev_element=nums[i];
-            max_Length=Math.max(max_Length,length);
+           
         }
-        return max_Length;
+        
+        return max_length;
     }
 }
